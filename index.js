ba5718de77b9b92d0a5b7b48b288287346c0e4c3
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-04-23 14:08:57 +0700

    [TNK-003] Complex Query And, Or Operations

--- index.js
+++ index.js
@@ -24,12 +24,13 @@ const User = mongoose.model("User", userSchema);
 // ne  - not equal
 // in  - include
 
-async function fetchInformation(){
-    // const users = await User.findById("6027d1fd8d4a543a2049d1a0");
-    // const users = await User.find({isMarried: false}).select("name salary").sort("-salary").limit(1);
-    // const users = await User.find({isMarried: false}).countDocuments();
-    const users = await User.find({salary: {$in: [50000, 50000000, 25000]}});
-    console.log(users);
+async function fetchInformation() {
+  // const users = await User.findById("6027d1fd8d4a543a2049d1a0");
+  // const users = await User.find({isMarried: false}).select("name salary").sort("-salary").limit(1);
+  // const users = await User.find({isMarried: false}).countDocuments();
+  // const users = await User.find({salary: {$in: [50000, 50000000, 25000]}});
+  const users = await User.find().and([{ isMarried: true }, { age: 30 }]);
+  console.log(users);
 }
 fetchInformation();
 
