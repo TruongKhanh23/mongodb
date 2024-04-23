bc694ec76a5a56c2db59f6d33c544977a51be04f
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-04-23 11:59:29 +0700

    [TNK-002] Schema and Models (Database, Table, Field)

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const mongoose = require("mongoose")
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-    .then(() => console.log("Database Is Connected"))
-    .catch((err) => console.log(err));
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("Database Is Connected"))
+  .catch((err) => console.log(err));
+
+// Schema (Shape of a Document)
+// Document, Collection, Database
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    isMarried: Boolean,
+    salary: Number,
+    gender: String,
+})
+const User = mongoose.model("User", userSchema)
